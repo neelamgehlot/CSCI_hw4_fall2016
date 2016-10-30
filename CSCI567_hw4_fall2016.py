8ad58f041efd3f6cb85168ba3f5a4407d47a18ba
neelamgehlot <gehlot.neelam1993@gmail.com>
2016-10-30 11:21:24 -0700

    Checking shapes of small dataset

--- CSCI567_hw4_fall2016.py
+++ CSCI567_hw4_fall2016.py
@@ -2,9 +2,13 @@ import hw_utils
 
 X_train, Y_train, X_test, Y_test = hw_utils.loaddata("MiniBooNE_PID.txt")
 
-X_train_normalized , X_test_normalized = hw_utils.normalize(X_train, X_test)
+X_train_norm , X_test_norm = hw_utils.normalize(X_train, X_test)
 
-print X_train.shape
-print Y_train.shape
-print X_test.shape
-print Y_test.shape
+X_train_small = X_train[0:1000]
+Y_train_small = Y_train[0:1000]
+
+X_train_norm_small = X_train_norm[0:1000]
+
+print X_train_small.shape
+print Y_train_small.shape
+print X_train_norm_small.shape
