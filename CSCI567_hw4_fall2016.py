438b099141a4663fed3b7d78f0e5539d2168805a
neelamgehlot <gehlot.neelam1993@gmail.com>
2016-10-30 11:39:31 -0700

    Part a execution on small dataset

--- CSCI567_hw4_fall2016.py
+++ CSCI567_hw4_fall2016.py
@@ -8,7 +8,38 @@ X_train_small = X_train[0:1000]
 Y_train_small = Y_train[0:1000]
 
 X_train_norm_small = X_train_norm[0:1000]
+X_test_norm_small = X_test_norm[0:100]
 
-print X_train_small.shape
-print Y_train_small.shape
-print X_train_norm_small.shape
+X_test_small = X_test[0:100]
+Y_test_small = Y_test[0:100]
+
+print "start"
+
+hw_utils.testmodels(X_train_small, Y_train_small, X_test_small, Y_test_small,
+                    [50, 2], 'linear', 'softmax',[0.0], 30, 1000, 0.001, 0.0, 0.0, False, False, 1)
+
+hw_utils.testmodels(X_train_small, Y_train_small, X_test_small, Y_test_small,
+                    [50, 50, 2], 'linear', 'softmax',[0.0], 30, 1000, 0.001, 0.0, 0.0, False, False, 1)
+
+hw_utils.testmodels(X_train_small, Y_train_small, X_test_small, Y_test_small,
+                    [50, 50, 50, 2], 'linear', 'softmax',[0.0], 30, 1000, 0.001, 0.0, 0.0, False, False, 1)
+
+hw_utils.testmodels(X_train_small, Y_train_small, X_test_small, Y_test_small,
+                    [50, 50, 50, 50, 2], 'linear', 'softmax',[0.0], 30, 1000, 0.001, 0.0, 0.0, False, False, 1)
+
+hw_utils.testmodels(X_train_small, Y_train_small, X_test_small, Y_test_small,
+                    [50, 50, 2], 'linear', 'softmax',[0.0], 30, 1000, 0.001, 0.0, 0.0, False, False, 1)
+
+hw_utils.testmodels(X_train_small, Y_train_small, X_test_small, Y_test_small,
+                    [50, 500, 2], 'linear', 'softmax',[0.0], 30, 1000, 0.001, 0.0, 0.0, False, False, 1)
+
+hw_utils.testmodels(X_train_small, Y_train_small, X_test_small, Y_test_small,
+                    [50, 500, 300, 2], 'linear', 'softmax',[0.0], 30, 1000, 0.001, 0.0, 0.0, False, False, 1)
+
+hw_utils.testmodels(X_train_small, Y_train_small, X_test_small, Y_test_small,
+                    [50, 800, 500, 300, 2], 'linear', 'softmax',[0.0], 30, 1000, 0.001, 0.0, 0.0, False, False, 1)
+
+hw_utils.testmodels(X_train_small, Y_train_small, X_test_small, Y_test_small,
+                    [50, 800, 800, 500, 300, 2], 'linear', 'softmax',[0.0], 30, 1000, 0.001, 0.0, 0.0, False, False, 1)
+
+print "end"
